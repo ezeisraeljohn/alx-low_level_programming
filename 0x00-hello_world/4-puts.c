c854be037441581a207041ad4c853de200ac023c
ezeisraeljohn <ezeisrael499@gmail.com>
2023-09-07 23:07:10 +0000

    Give output using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * Uses the puts function
+ *
+ * Return: Always (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
