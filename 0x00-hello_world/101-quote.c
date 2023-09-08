e72ad4c5c16e59fb8b47836d58a35951ee4e15a6
ezeisraeljohn <ezeisrael499@gmail.com>
2023-09-08 00:52:23 +0000

    Output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry point
@@ -8,7 +7,7 @@
 int main(void)
 {
 	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, 59); 
+	write(2, message, 59);
 	return (1);
 }
 
