3e1f21d40946268e0ca4cfe89c74d4dc09700c52
ezeisraeljohn <ezeisrael499@gmail.com>
2023-09-08 00:47:08 +0000

    Output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
  * main - Entry point
  *
  * Return: Always 1 (Success)
- */ 
-
-int main(void) 
+ */
+int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	 ssize_t num_bytes = write(2, message, 59);
-	return (1); 
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, 59); 
+	return (1);
 }
+
 
