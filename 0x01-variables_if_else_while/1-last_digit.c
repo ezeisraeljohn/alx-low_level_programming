72b7d2f7e087c6016729583619a7dc6c3461de51
ezeisraeljohn <ezeisrael499@gmail.com>
2023-09-08 22:57:26 +0000

    Prints single digit extracted from a number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,10 +15,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	lastNum = n%10;
+	lastNum = n %i 10;
 	if (lastNum > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n,lastNum);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastNum);
 	}
 	else if (lastNum == 0)
 	{
