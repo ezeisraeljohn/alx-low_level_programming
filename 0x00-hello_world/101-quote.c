7975f5b545923bd66eb53129d59d7fc9355aa03e
ezeisraeljohn <ezeisrael499@gmail.com>
2023-09-08 00:38:05 +0000

    No error output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
  * Return: Always 1 (Success)
- */
-int main(void)
+ */ 
+
+int main(void) 
 {
-	fwrite(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	 ssize_t num_bytes = write(2, message, 59);
+	 if (num_bytes == -1) 
+	 {
+		 return 1; 
+	 }
 }
+
