f7e4cc34a0bd3f208797ed81ef17c3484504ed92
ezeisraeljohn <ezeisrael499@gmail.com>
2023-09-08 23:54:11 +0000

    Prints Alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Convert all letters to lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+		putchar(c++);
+	putchar('\n');
+
+	return (0);
+}
+
+	
+
