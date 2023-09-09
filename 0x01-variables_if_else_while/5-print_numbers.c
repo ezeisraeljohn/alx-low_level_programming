78f55369ceaf0b63f62245671172c3544d5e3c9f
ezeisraeljohn <ezeisrael499@gmail.com>
2023-09-09 01:08:09 +0000

    All decimal digits printed out

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all digits from
+ * One to Ten
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for(i = 0; i <= 9; i++)
+		printf("%d", i);
+	return (0);
+}
