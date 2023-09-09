ff88be25bd4ae68e43f077894453f837ee8d45d2
ezeisraeljohn <ezeisrael499@gmail.com>
2023-09-09 00:39:24 +0000

    Prints all letters of the alphabet minus e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - This prints out the all alphabets
+ * with exclusion of a and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c < 'z'; c++)
+	{
+		if ((c == 'e') || (c == 'q'))
+			continue;
+		else 
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
