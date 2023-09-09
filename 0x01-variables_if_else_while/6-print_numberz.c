426a324545c250cbd96a39993b5d556bd98bf97a
ezeisraeljohn <ezeisrael499@gmail.com>
2023-09-09 01:29:36 +0000

    Done

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Prints integer with putchar
+ * main - prints integer with putchar
  *
  * Remain: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
+
 	for (i = 0; i <= 9; i++)
 		putchar(i + '0');
 	putchar('\n');
