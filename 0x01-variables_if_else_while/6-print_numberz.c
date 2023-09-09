5af0a647c0cad330afe475297bf509cccd46cd45
ezeisraeljohn <ezeisrael499@gmail.com>
2023-09-09 01:25:13 +0000

    Putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * main - Prints out all digits of 
- * decimals 
+ * main - Prints integer with putchar
  *
- * Return: Always 0 (Success)
+ * Remain: Always 0 (Success)
  */
 int main(void)
 {
