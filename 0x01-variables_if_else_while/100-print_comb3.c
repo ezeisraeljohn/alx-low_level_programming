d4891873de5fe0686e209cbc669d1fe8cef6ca4e
ezeisraeljohn <ezeisrael499@gmail.com>
2023-09-11 21:51:29 +0000

    Prints combo digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,18 +15,16 @@ int main(void)
 	{
 		for (j = i + 1; j <= 9; j++)
 		{
-			if (i != j)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-			}
-			if ((i != '8') && (j != '9'))
+			putchar(i + '0');
+			putchar(j + '0');
+			
+			if (i < 8 )
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
+	putchar('\n');
 	return (0);
 }
-
