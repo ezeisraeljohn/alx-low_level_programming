ccca397775539f75cb5525929a47499504422e1b
ezeisraeljohn <ezeisrael499@gmail.com>
2023-09-11 22:31:07 +0000

    Prints combo digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,9 +9,26 @@ int main(void)
 {
 	int i;
 	int j;
+	int k;
 	
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j< 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
-			
+			for (k = j + 1; k <= 9; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+
+				if (i < 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}
+		
