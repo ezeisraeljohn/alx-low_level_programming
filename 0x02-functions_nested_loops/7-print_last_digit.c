f55400bfcd9c23ba24d1e7707df7c3a451ca41e4
Eze Israel John <ezeisrael499@gmail.com>
2023-09-13 13:54:53 -0700

    Returns last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+/**
+ * print_last_digit - This function prints
+ * the last digit
+ * @a: The operand
+ *
+ * Return: return the last number
+ */
+int print_last_digit(int a)
+{
+	int b;
+
+	b = a % 10;
+	return (b);
+}
