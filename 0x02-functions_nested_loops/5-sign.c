6d05cf1c44bc58e5cc517b0a4a849e5f5eed45e6
Eze Israel John <ezeisrael499@gmail.com>
2023-09-13 08:27:52 -0700

    Checks for positive numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_sign - checks for positive numbers
+ * @n: operand
+ *
+ * Return: b
+ */
+int print_sign(int n)
+{
+	int b;
+	
+	if(n > 0)
+	{
+		b = 1;
+		_putchar('+');
+		return (b);
+	}
+	else if (n == 0)
+	{
+		b = 0;
+		_putchar('0');
+		return (b);
+	}
+	else
+	{
+		b = -1;
+		_putchar('-');
+		return (b);
+	}
+}
