d757d7d47ee9b78ff941ba8e4380a082b040290c
Eze Israel John <ezeisrael499@gmail.com>
2023-09-13 07:49:00 -0700

    Checks for alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabets
+ * @c: Operand
+ *
+ * Return b
+ */
+int _isalpha(int c)
+{
+	char b;
+
+	if (((c >= 'a') && ( c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+		b = 1;
+	else
+		b = 0;
+	return (b);
+}
