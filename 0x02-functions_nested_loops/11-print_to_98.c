1e9913d5585cb1ae509182ac693ae5e728e0ad5e
Eze Israel John <ezeisrael499@gmail.com>
2023-09-13 16:37:49 -0700

    Print numbers to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Prints to 98
+ * @n: the parameter needed
+ *
+ * Return: the output
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 99)
+		{
+			_putchar(n + '0');
+			_putchar(',');
+			_putchar(' ');
+			n--;
+		}
+	else
+	{
+		while (n < 99)
+		{
+			_putchar(n + '0');
+			_putchar(',');
+			_putchar(' ');
+			n++;
+		}
+	}
+
+}
