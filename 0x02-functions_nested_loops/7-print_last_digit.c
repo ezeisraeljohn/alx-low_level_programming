ff43ef99d9dd607fa5be11f898bd13cd53f39b2a
Eze Israel John <ezeisrael499@gmail.com>
2023-09-13 15:33:48 -0700

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,20 +10,10 @@
 int print_last_digit(int a)
 {
 	int b;
+	int c;
 
-	if (a >= 0)
-	{
-		b = a % 10;
-	}
-	else if (a = –2147483648)
-	{
-		b = 8;
-	}
-	else
-	{
-		a = -a;
-		b = a % 10;
-	}
+	c = a < 0 ? -a : a;
+	b = c % 10;
 	_putchar(b + '0');
 	return (b);
 }
