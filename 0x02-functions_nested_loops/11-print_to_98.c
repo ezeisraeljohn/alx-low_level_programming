3db8cf237a4d8e9f325370b2bfda100a03de271c
Eze Israel John <ezeisrael499@gmail.com>
2023-09-13 17:02:37 -0700

    Print numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,19 +11,21 @@ void print_to_98(int n)
 {
 	if (n >= 98)
 	{
-		while (n >= 98)
+		while (n > 98)
 		{
 			printf("%d, ", n);
 			n--;
 		}
+		printf("%d", 98);
 	}
 	else
 	{
-		while (n <= 98)
+		while (n < 98)
 		{
 			printf("%d, ", n);
 			n++;
 		}
+		printf("%d", 98);
 	}
 }
 
