223fc4c31e845760b4851226a9d60f6ca7105106
Eze Israel John <ezeisrael499@gmail.com>
2023-09-15 10:03:41 -0700

    Prints all digits except 2and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints out all the numbers
+ * except 2 and 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if ((i == 2) || (i == 4))
+			continue;
+		else
+			_putchar(i);
+		_putchar('\n');
+	}
+}
