9116fd235f4e9e78a55d86d3928d2b54d1c95ddc
Eze Israel John <ezeisrael499@gmail.com>
2023-09-15 14:03:05 -0700

    print lines to the terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,7 +12,7 @@ void print_line(int n)
 
 	for (i = 0; i <= n; i++)
 	{
-		if (n == 0)
+		if (n <= 0)
 		{
 			_putchar('\n');
 			break;
@@ -21,8 +21,8 @@ void print_line(int n)
 		{
 			_putchar('_');
 		}
-	} 
-	if (n != 0)
+	}
+	if !(n <= 0)
 	{
 		_putchar('\n');
 	}
