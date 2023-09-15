e613115eb9469b5b06e1ac72a9bf4de2609b6b80
Eze Israel John <ezeisrael499@gmail.com>
2023-09-15 14:56:52 -0700

    Prints diagonally

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - the slashes diagonally
+ *@n: parameter used
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		if (n > 0)
+			_putchar('\\');
+		else
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
