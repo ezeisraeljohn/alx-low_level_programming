85e8a8fcb41a0edacbd515e9bdf944a84e9fae6c
Eze Israel John <ezeisrael499@gmail.com>
2023-09-15 09:48:13 -0700

    Prints out all digits

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints all digits
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
