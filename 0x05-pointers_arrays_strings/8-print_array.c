1468914ffa491105578e9b1ab4c247ddae3374b8
Eze Israel John <ezeisrael499@gmail.com>
2023-09-18 18:24:27 -0700

    Prints out elements of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,6 +13,9 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < n; i++)
-		printf("%d, ", *(a + i));
+	{	printf("%d", *(a + i));
+		if (i < (n - 1))
+			printf(", ");
+	}
 	_putchar('\n');
 }
