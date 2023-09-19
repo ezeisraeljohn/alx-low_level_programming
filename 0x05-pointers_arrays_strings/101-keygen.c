6be8ac657ded32ffb5609c68b4d5b0549cbe19da
Eze Israel John <ezeisrael499@gmail.com>
2023-09-18 19:17:07 -0700

    done

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,22 +5,27 @@
 #define PASSWORD_LENGTH 10
 
 /**
- * generate_password - This generates random passwords
- * @password: operand 1
- * @length: operand 2
+ * generate_password -  Generates a random password based on the provided character set.
+ * @password: The buffer to store the generated password.
+ * @length: The length of the password to be generated.
  *
- * Return: Nothing
+ * Retrun: Nothing
  */
+void generate_password(char *password, int length);
+
+int main();
+
 void generate_password(char *password, int length) {
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    int charset_size = sizeof(charset) - 1;
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	int charset_size = sizeof(charset) - 1;
+	int i;
 
-    for (int i = 0; i < length; i++) {
-        int index = rand() % charset_size;
-        password[i] = charset[index];
-    }
+	for (i = 0; i < length; i++) {
+		int index = rand() % charset_size;
+		password[i] = charset[index];
+	}
 
-    password[length] = '\0'; 
+	password[length] = '\0';
 }
 
 /**
@@ -28,14 +33,15 @@ void generate_password(char *password, int length) {
  *
  * Return: Always 0 (Success)
  */
+
 int main() {
-    char password[PASSWORD_LENGTH + 1];  
-    srand(time(NULL)); 
+	char password[PASSWORD_LENGTH + 1];
+	srand(time(NULL));
 
-    generate_password(password, PASSWORD_LENGTH);
+	generate_password(password, PASSWORD_LENGTH);
 
-    printf("Generated Password: %s\n", password);
+	printf("Generated Password: %s\n", password);
 
-    return (0);
+	return 0;
 }
 
