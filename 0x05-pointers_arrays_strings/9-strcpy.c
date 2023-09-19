c9aa6a3aaf183db25292c740407a2cc457acf1ec
Eze Israel John <ezeisrael499@gmail.com>
2023-09-18 18:36:41 -0700

    Mimicks the strcpy function

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies the string pointed to by src,
+ *     including the terminating null
+ * @dest: The destination string
+ * @src: The operand used
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int j;
+
+	for (j = 0; src[j]; j++)
+		dest[j] = src[j];
+	dest[i] = '\0';
+	return (dest);
+}
