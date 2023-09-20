e507421865e16ad93f024d9847215dfce6e49dbb
Eze Israel John <ezeisrael499@gmail.com>
2023-09-19 17:14:27 -0700

    Thats it

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,30 +8,38 @@
 #define CHECKSUM 1277  
 
 char *generatePassword(int *sum) {
-	const char alphanum[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char *password = malloc(PASSWORD_LENGTH + 1);
-	if (password == NULL) {
-		fprintf(stderr, "Memory allocation failed.\n");
-		exit(EXIT_FAILURE);
-	}
+    const char alphanum[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    char *password = malloc(PASSWORD_LENGTH + 1);
 
-	srand(time(NULL));
-	int i;  // Move variable declaration to the beginning of the block
-	for (i = 0; *sum < CHECKSUM; ++i) {
-		password[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
-		*sum += (int) password[i];
-	}
+    /* Check if memory allocation failed */
+    if (password == NULL) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        exit(EXIT_FAILURE);
+    }
 
-	password[CHECKSUM] = '\0';  /* Null-terminate the password */
-	return password;
+    srand(time(NULL));
+    int i;  /* Move variable declaration to the beginning of the block */
+    for (i = 0; *sum < CHECKSUM; ++i) {
+        password[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
+        *sum += (int) password[i];
+    }
+
+    password[CHECKSUM] = '\0';  /* Null-terminate the password */
+    return password;
 }
 
 int main() {
-	int sum = 0;
-	char *password = generatePassword(&sum);
+    int sum = 0;
+    char *password = generatePassword(&sum);
 
-	// Use the password variable or remove the declaration if not needed
+    /* Use the password variable or remove the declaration if not needed */
 
-	return 0;
+    /* Ensure the variable is used to avoid the unused variable warning */
+    if (password) {
+        printf("Generated password: %s\n", password);
+        free(password);  /* Free the allocated memory */
+    }
+
+    return 0;
 }
 
