35a045fef30eef9faee83e14b749308f59567cbc
Eze Israel John <ezeisrael499@gmail.com>
2023-09-19 17:09:39 -0700

    Checks for passwords

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,46 +2,36 @@
 #include <time.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdio.h>
 
 #define PASSWORD_LENGTH 100
 #define CHECKSUM 1277  
 
-/**
- * generatePassword - A function that generates a random password and places it
- * inside an allocated variable named password
- * @sum: A pointer to a variable that keeps track of the generated password
- *       ASCII value sum.
- * Return: The allocated char* password
- */
 char *generatePassword(int *sum) {
-    const char alphanum[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    char *password = malloc(PASSWORD_LENGTH + 1);
-    if (password == NULL) {
-        fprintf(stderr, "Memory allocation failed.\n");
-        exit(EXIT_FAILURE);
-    }
+	const char alphanum[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *password = malloc(PASSWORD_LENGTH + 1);
+	if (password == NULL) {
+		fprintf(stderr, "Memory allocation failed.\n");
+		exit(EXIT_FAILURE);
+	}
 
-    srand(time(NULL));
-    for (int i = 0; *sum < CHECKSUM; ++i) {
-        password[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
-        *sum += (int) password[i];
-    }
+	srand(time(NULL));
+	int i;  // Move variable declaration here for older C standards
+	for (i = 0; *sum < CHECKSUM; ++i) {
+		password[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
+		*sum += (int) password[i];
+	}
 
-    password[CHECKSUM] = '\0';  // Null-terminate the password
-    return password;
+	password[CHECKSUM] = '\0';  // Null-terminate the password
+	return password;
 }
 
 int main() {
-    int sum = 0;
-    char *password = generatePassword(&sum);
+	int sum = 0;
+	char *password = generatePassword(&sum);
 
-    // Print the generated password
-    printf("Generated Password: %s\n", password);
-    printf("Checksum: %d\n", sum);
+	// Add the rest of your main function logic here
 
-    // Free the allocated memory for the password
-    free(password);
-
-    return 0;
+	return 0;
 }
 
