feccff3a83643077239b23f29b8402988d026c71
Eze Israel John <ezeisrael499@gmail.com>
2023-09-22 13:40:00 -0700

    reverses string

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,22 +1,26 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * reverse_array - Reverses content of an array
- * @a: array in question
- * @n: The number of arrays
- *
- * Return: Nothing
+ * _swap - swaps the values of two integers
+ * @a: pointer to an integer
+ * @b: pointer to an integer
+ */
+static void _swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: array of integers
+ * @n: number of elements in the array
  */
 void reverse_array(int *a, int n)
 {
-	int i;
+	int i, j;
 
-	for (i = n - 1; i >= 0; i--)
-	{
-		if (i != (n - 1))
-			printf(", ");
-		printf("%d", a[i]);
-	}
-
+	for (i = 0, j = n - 1; i < j; i++, j--)
+		_swap(&a[i], &a[j]);
 }
