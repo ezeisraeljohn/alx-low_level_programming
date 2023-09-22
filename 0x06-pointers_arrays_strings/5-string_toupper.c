3373c580e79afba573fdffb21bb96852c773c3f9
ezeisraeljohn <ezeisrael499@gmail.com>
2023-09-22 19:21:44 +0100

    Converts strings to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+/**
+ * string_toupper - turns strings to uppercase
+ * @s: parameter to be used
+ * 
+ * Return: returns the lowwrcase letter
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+	}
+	return (s);
+}
