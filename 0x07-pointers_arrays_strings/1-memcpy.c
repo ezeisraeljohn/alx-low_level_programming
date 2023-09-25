f3f53fe0525c8a1ee3f6b1b4747ef04b73356c23
ezeisraeljohn <ezeisrael499@gmail.com>
2023-09-25 18:46:56 +0100

    temporal changes

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _memcpy - Copies byte from source to 
+ * destination
+ * @dest: Desination memory area
+ * @src: Source memory area
+ * @n: number of bytes to be copied
+ *
+ * Return: The copied memory area
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,4 +3,5 @@
 
 int _putchar(int c);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
