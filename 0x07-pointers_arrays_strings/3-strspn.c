2c54b6791442117bcd9a836df5ab7eec776256f4
Eze Israel John <ezeisrael499@gmail.com>
2023-09-26 00:23:47 +0100

    Done

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,28 +1,34 @@
 #include "main.h"
 
 /**
- * _strspn - Returns the number of byte of the string present
- * @s: pointer to the distination string
- * @accept: pointer to the second string
+ * inCharSet - checks if a character is in a set of characters
+ * @c: character to be checked
+ * @accept: string containing the characters to match
+ * Return: 1 if c is in accept, 0 otherwise
+ */
+int inCharSet(char c, char *accept)
+{
+	int j = 0;
+
+	while (accept[j])
+		if (c == accept[j++])
+			return (1);
+	return (0);
+}
+
+/**
+ *  _strspn - gets the length of a prefix substring
+ *  @s: string to be scanned
+ *  @accept: string containing the characters to match
  *
- * Return the number of bytes in the initial segment of s
+ *  Return: number of bytes in the initial segment of s which consist only of
+ *  bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 1;
+	unsigned int j = 0;
 
-	while (*s)
-	{
-		while (*s && *accept)
-		{
-			if (*s == *accept)
-			{
-				i++;
-			accept++;
-			}
-			s++;
-		}
-		s++;
-	}
-	return (i);
+	while (s[j] && inCharSet(s[j], accept))
+		j++;
+	return (j);
 }
