c59cd2f9ecfee50aa680b8994ac280634a4580c8
Eze Israel John <ezeisrael499@gmail.com>
2023-09-25 20:56:46 +0100

    Copies byte from source to destination

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,11 +11,10 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 
-	unsigned int i;
+	char *original_dest = dest;
 
-	for (i = 0; i < n; i++)
-	{
+	while (n--)
 		*dest++ = *src++;
-	}
-	return (dest);
+
+	return (original_dest);
 }
