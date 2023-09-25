bc8464a3d973a7bb388458e34f0580ebee6e28bf
Eze Israel John <ezeisrael499@gmail.com>
2023-09-25 23:57:46 +0100

    Returns the byte of matching strings in substrings

--- 0x07-pointers_arrays_strings/3-main.c
+++ 0x07-pointers_arrays_strings/3-main.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <stddef.h>
 
 /**
  *  * main - check the code
@@ -9,14 +8,11 @@
  *     */
 int main(void)
 {
-	char *s = "hello";
-	char *f;
+	char *s = "hello, world";
+	char *f = "oleh";
+	unsigned int n;
 
-	f = _strchr(s, 'r');
-
-	if (f != NULL)
-	{
-		printf("%s\n", f);
-	}
+	n = _strspn(s, f);
+	printf("%u\n", n);
 	return (0);
 }
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strspn - Returns the number of byte of the string present
+ * @s: pointer to the distination string
+ * @accept: pointer to the second string
+ *
+ * Return the number of bytes in the initial segment of s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 1;
+
+	while(*s)
+	{
+		while (*s && *accept)
+		{
+			if(*s == *accept)
+			{
+				i++;
+			accept++;
+			}
+			s++;
+		}
+		s++;
+	}
+	return (i);
+}
