ca4dfbc47f1db74435454223d5faa5cc464de191
Eze Israel John <ezeisrael499@gmail.com>
2023-10-04 20:51:25 +0100

    Returns a pointer to an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *ptr = (char *)malloc(size * sizeof(char));
 
-	if (size == 0)
+	if (size <= 0)
 		return (NULL);
 
 	if (ptr == NULL)
@@ -23,9 +23,7 @@ char *create_array(unsigned int size, char c)
 
 	for (i = 0; i < size; i++)
 	{
-		*ptr = c;
-		ptr++;
+		*(ptr + i) = c;
 	}
 	return (ptr);
-	free(ptr);
 }
