3ee3631cf7a02db3e78bb4892e9332bddbfa939c
Eze Israel John <ezeisrael499@gmail.com>
2023-10-10 22:07:36 +0100

    Creates a C structure for a dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * struct dog - defines the structure of a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
