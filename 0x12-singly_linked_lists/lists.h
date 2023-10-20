6c99f3d17fa4e103e90b1169eae49b4b01c178c5
Eze Israel John <ezeisrael499@gmail.com>
2023-10-20 10:35:06 +0100

    Added the header file for the prototypes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,9 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LIST_H
+#define LIST_H
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -20,13 +20,16 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-int _putchar(char c);
-int _strlen(char *s);
+/* linked list function prototypes */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void _constructor(void) __attribute__ ((constructor));
 
-#endif
+/* a safer way to deallocate dynamic memory */
+void _free(void **ptr);
+#define safe_free(p) _free((void **) &(p))
+
+#endif /* LIST_H */
+
