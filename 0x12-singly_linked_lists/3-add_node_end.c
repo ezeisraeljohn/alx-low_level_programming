6c4a2fec9bc8fb04a20d3e54f44de2aed31c5698
Eze Israel John <ezeisrael499@gmail.com>
2023-10-20 10:55:33 +0100

    Added a function that add a new node at the end of a list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,35 +1,46 @@
-#include "lists.h"
+nclude "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a list_t list
- * @head: pointer to the head node
- * @str: the string data
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: a pointer to the head node
+ * @str: the string data to add
  *
- * Return: the address of the new element, or NULL if it failed
+ * Return: the address of the new node, or NULL if it failed
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
+	list_t *new_node, *tail;
 	char *dup_str;
+
+	tail = *head;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-	{
-		return (NULL); /* memory allocation for new node failed */
-	}
+		return (NULL);
 
 	dup_str = strdup(str);
 	if (dup_str == NULL)
 	{
-		free(new_node);
-		return (NULL); /* string duplication failed */
+		free(new_node); /* clean up memory and return NULL */
+		return (NULL);
 	}
 
-	/* add new node to the list and adjust links */
 	new_node->str = dup_str;
 	new_node->len = strlen(dup_str);
-	new_node->next = *head;
-	*head = new_node;
+	new_node->next = NULL;
+
+	/* check if the list is empty - guarantees an O(1) */
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		/* list is not empty, traverse to the tail. O(n) */
+		while (tail->next != NULL)
+		{
+			tail = tail->next;
+		}
+		tail->next = new_node; /* adjust tail links */
+	}
 
 	return (*head);
 }
