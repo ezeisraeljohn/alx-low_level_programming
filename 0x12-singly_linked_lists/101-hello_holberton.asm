329de2958178f8ba2c61e5e76cc890f331105a4e
Eze Israel John <ezeisrael499@gmail.com>
2023-10-20 11:32:34 +0100

    Program in assembly that prints Hello, Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,28 +1,16 @@
+section .data
+hello db "Hello, Holberton", 10, 0  ; The message to be printed with a newline character
+
+section .text
 global main
 extern printf
 extern exit
 
-section .data
-message db "Hello, Holberton", 10, 0
-
-section .text
 main:
-push rdi
-mov rdi, message
-call printf
-pop rdi
+mov rdi, hello   ; Load the address of the hello message into rdi
+call printf      ; Call the printf function with the message address as an argument
 
-; check the return value from printf
-cmp rax, -1
-je .error_handler ; error occured, handle it
-
-; exit the program gracefully with code 0
+; Exit the program gracefully with code 0
 mov rdi, 0
 call exit
 
-.error_handler:
-; handle errors that occured during the call to printf
-
-; exit with 1
-mov rdi, 1
-call exit
