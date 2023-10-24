fa1e4e69be13790baf5a6d46e6cef7e5f7c76d49
Eze Israel John <ezeisrael499@gmail.com>
2023-10-25 00:29:51 +0100

    Added a function that frees a list wand return its size

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,38 +1,77 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - frees a linked list
- * @h: pointer to the first node in the linked list
+ * detect_and_break_loop - Detects and breaks a loop in a linked list.
+ * @h: Pointer to the first node in the linked list.
  *
- * Return: number of elements in the freed list
+ * Return: The first node in the loop, if detected; otherwise, NULL.
+ */
+listint_t *detect_and_break_loop(listint_t **h)
+{
+	listint_t *slow = *h, *fast = *h;
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		/* Check for a loop */
+		if (slow == fast)
+		{
+			slow = *h;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			fast = fast->next; /**
+					    *Move fast to the next node
+					    * to keep one reference in the loop
+					    */
+
+			/* Break the loop */
+			fast->next = NULL;
+			return (fast);
+		}
+	}
+
+	return (NULL);
+}
+
+/**
+ * free_listint_safe - Safely frees a linked list with potential loops.
+ * @h: Pointer to the first node in the linked list.
+ *
+ * Return: Number of elements in the freed list.
  */
 size_t free_listint_safe(listint_t **h)
 {
 	size_t len = 0;
-	int diff;
-	listint_t *temp;
+	listint_t *loop_start;
 
 	if (!h || !*h)
 		return (0);
 
-	while (*h)
+	loop_start = detect_and_break_loop(h);
+
+	/* Free the non-loop part of the list */
+	while (*h != loop_start)
 	{
-		diff = *h - (*h)->next;
-		if (diff > 0)
-		{
-			temp = (*h)->next;
-			*h = temp;
-			len++;
-		}
-		else
-		{
-			*h = NULL;
-			len++;
-			break;
-		}
+		listint_t *temp = *h;
+		*h = (*h)->next;
+		free(temp);
+		len++;
 	}
 
-	*h = NULL;
+	/* Free the loop part of the list */
+	while (*h != NULL)
+	{
+		listint_t *temp = *h;
+		*h = (*h)->next;
+		free(temp);
+		len++;
+	}
 
 	return (len);
 }
+
