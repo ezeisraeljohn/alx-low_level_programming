b9b0a329dc8df1e5dbb9bde160e8b34255f20a19
Eze Israel John <ezeisrael499@gmail.com>
2023-10-27 00:16:06 +0100

    Added a function that converts a binary to a decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,22 +1,37 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number string to unsigned int
- * @b: the binary unmber as a string
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
  *
- * Return: the unsigned int form of b
+ * Return: unsigned int.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
+	unsigned int ui;
+	int len, base_two;
 
 	if (!b)
 		return (0);
-	while (*b)
+
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (*b != '0' && *b != '1')
+		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
-		num = num * 2 + (*b++ - '0');
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
-	return (num);
+
+	return (ui);
 }
