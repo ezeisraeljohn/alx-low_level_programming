4d120a930e031f343df929fd26e27392f52c8d24
Eze Israel John <ezeisrael499@gmail.com>
2023-12-12 00:55:34 +0100

    Added the file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-wget -P /tmp
-https://github.com/ezeisraeljohn/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libforce.so
+wget -P /tmp https://github.com/ezeisraeljohn/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libforce.so
 export LD_PRELOAD=/tmp/libforce.so
