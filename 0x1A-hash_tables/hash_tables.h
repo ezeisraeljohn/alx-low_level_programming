bd232f51d540c7598ea8d56290cc0d9cb7b46846
Eze Israel John <ezeisrael499@gmail.com>
2023-12-22 20:53:49 +0100

    Some changes added

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -87,5 +87,7 @@ void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
+shash_table_t *shash_table_create(unsigned long int size);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 
 #endif
