6f87c49c91cbcac268bcf74a2fb5f5af64380b67
Eze Israel John <ezeisrael499@gmail.com>
2023-12-22 14:59:57 +0100

    Added a function that creates a table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,28 +11,28 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_table = NULL;
-    unsigned long int i;
+	hash_table_t *new_table = NULL;
+	unsigned long int i;
 
-    /* Allocate memory for the hash table structure */
-    new_table = malloc(sizeof(hash_table_t));
-    if (new_table == NULL)
-        return (NULL);
+	/* Allocate memory for the hash table structure */
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
 
-    /* Allocate memory for the array of hash_node_t pointers */
-    new_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (new_table->array == NULL)
-    {
-        free(new_table);
-        return (NULL);
-    }
+	/* Allocate memory for the array of hash_node_t pointers */
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
 
-    /* Initialize each element of the array to NULL (no nodes in the lists yet) */
-    for (i = 0; i < size; i++)
-        new_table->array[i] = NULL;
+	/* Initialize each element of the array to NULL (no nodes in the lists yet) */
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
 
-    /* Set the size of the hash table */
-    new_table->size = size;
+	/* Set the size of the hash table */
+	new_table->size = size;
 
-    return (new_table);
+	return (new_table);
 }
